f4ec2a49411ffb7d7a2b56f44c97e17c73a1d7b5
Sengming <nitricrage@gmail.com>
2018-03-01 01:06:45 -0500

    Disabled memory bounds check in do_sys_read call and added debug print statement. Build is ok

--- fs/read_write.c
+++ fs/read_write.c
@@ -447,8 +447,8 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
 		return -EINVAL;
-	if (unlikely(!access_ok(VERIFY_WRITE, buf, count)))
-		return -EFAULT;
+	//if (unlikely(!access_ok(VERIFY_WRITE, buf, count)))
+	//	return -EFAULT;
 
 	ret = rw_verify_area(READ, file, pos, count);
 	if (ret >= 0) {
--- kernel/popcorn/fs_server.c
+++ kernel/popcorn/fs_server.c
@@ -98,6 +98,7 @@ ssize_t send_file_read_request(unsigned int fd, size_t count, int origin_nid, ch
         goto out_fail;
     }
 
+    printk("send_file_read_request, before wait at station\n");
     /* Ensure first read request has completed before sending another*/
     rep = wait_at_station(ws);
 	put_wait_station(ws);
@@ -105,7 +106,7 @@ ssize_t send_file_read_request(unsigned int fd, size_t count, int origin_nid, ch
     if (!WARN_ON(rep == NULL)){
         copy_to_user(user_buf, rep->buf, rep->read_len);
     }
-    
+    printk("send_file_read_request, after wait at station\n");
     return rep->read_len;
 
 out_fail:
@@ -124,7 +125,8 @@ int process_remote_read_req(struct pcn_kmsg_message *msg)
     rep->origin_ws = req->origin_ws;
     rep->fd = req->fd;
     rep->read_len = do_sys_file_read(req->fd, rep->buf, req->read_len);
-    
+    printk("rep->read_len: %d\n", rep->read_len);
+    printk("remonte nid: %d\n", current->remote_nid);
     if (rep->read_len > 0) {
         pcn_kmsg_send(current->remote_nid, rep, sizeof(*rep));
     }
@@ -133,12 +135,12 @@ int process_remote_read_req(struct pcn_kmsg_message *msg)
     return ret;
 }
 
-static int handle_remote_read_reply(struct pcn_kmsg_message *msg)
+static inline int handle_remote_read_reply(struct pcn_kmsg_message *msg)
 {
     int ret = 0;
     remote_read_reply_t *rep = (remote_read_reply_t*) msg;
-   
     struct wait_station* ws = wait_station(rep->origin_ws);
+    printk("Handle remote read reply called\n");   
 	ws->private = rep->buf;
 	smp_mb();
 
@@ -147,7 +149,6 @@ static int handle_remote_read_reply(struct pcn_kmsg_message *msg)
 
     return ret;
 }
-EXPORT_SYMBOL(handle_remote_read_reply);
 
 DEFINE_KMSG_RW_HANDLER(remote_read_req, remote_read_req_t, origin_pid);
 
